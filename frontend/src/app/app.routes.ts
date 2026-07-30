b45c5dee713a284aacfbf10b7ffa20a89edaf3e4
MANSI RAJU SHINDE <mansishinde087@gmail.com>
2026-07-30 21:40:38 +0530

    Week 1 Milestone - Register & Login - MANSI RAJU SHINDE

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,27 @@
 import { Routes } from '@angular/router';
 
-
 export const routes: Routes = [
-  
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./features/auth/auth').then(
+        m => m.Auth
+      )
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./features/auth/auth').then(
+        m => m.Auth
+      )
+  },
+  {
+    path: '**',
+    redirectTo: 'login'
+  }
 ];
