531eb2b66be946f75733c95109b76c533ba7c4fb
Roshan-Kumar071 <kroshan42342@gmail.com>
2026-08-02 17:30:55 +0530

    feat(tax): implement tax estimation APIs

--- src/app.js
+++ src/app.js
@@ -10,11 +10,13 @@ const authRoutes = require("./routes/auth.routes");
 const userRoutes = require("./routes/user.routes");
 const transactionRoutes = require("./routes/transaction.routes");
 const budgetRoutes = require("./routes/budget.routes");
+const taxRoutes = require("./routes/tax.routes");
 
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/transactions", transactionRoutes);
 app.use("/api/budgets", budgetRoutes);
+app.use("/api/tax-estimates", taxRoutes);
 
 // Test Route
 app.get("/", (req, res) => {
