096df8d886925a0f81b7b1ec8b43d73cc6c3a720
Aastha Malviya <141214756+aastha44ma@users.noreply.github.com>
2026-08-04 19:45:37 +0530

    Delete src directory

--- src/routes/user.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const authenticateUser = require("../middleware/auth.middleware");
-const { getUserProfile } = require("../controllers/userController");
-
-router.get("/profile", authenticateUser, getUserProfile);
-
-module.exports = router;
