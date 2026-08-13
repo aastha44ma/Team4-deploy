ec0aa0afd30e886ccc1b291c6a961b73536301d8
Aastha Malviya <141214756+aastha44ma@users.noreply.github.com>
2026-08-13 19:27:05 +0530

    Refactor authGuard to include token check

--- frontend/src/app/guards/auth.guard.ts
+++ frontend/src/app/guards/auth.guard.ts
@@ -2,18 +2,25 @@ import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
 import { ApiService } from '../services/api';
 
+
 export const authGuard: CanActivateFn = () => {
+
   const router = inject(Router);
   const apiService = inject(ApiService);
 
-  
+
   const token = apiService.getToken();
 
+
   if (token) {
+
     return true;
+
   }
 
+
   router.navigate(['/login']);
+
   return false;
-  
+
 };
