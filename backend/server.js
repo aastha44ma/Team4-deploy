6d29e0967f594ba7364bf02ff23aeff25ef6e26f
Aastha Malviya <141214756+aastha44ma@users.noreply.github.com>
2026-08-25 20:07:31 +0530

    Server.js changes

--- backend/server.js
+++ backend/server.js
@@ -7,3 +7,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`✅ Server running on http://localhost:${PORT}`);
 });
+
+// Required for Vercel deployment
+module.exports = app;
